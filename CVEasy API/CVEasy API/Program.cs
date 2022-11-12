449a7c383ba2c17a0a9c8f70cf17c36206457397
UnchartedWorld <nathangvjcollins@gmail.com>
2022-11-12 23:57:49 +0000

    feat(Program): Connects DB to Program.cs, utilises Swagger
    This should've been similar to the default, but I somehow managed to lose that and created an unusable Program.cs. This one now works and will serve as a temporary solution for checking if my API is RESTful.

--- CVEasy API/CVEasy API/Program.cs
+++ CVEasy API/CVEasy API/Program.cs
@@ -1,6 +1,35 @@
+using CVEasy_API.Data;
+using Microsoft.EntityFrameworkCore;
+using CVEasy_API.Model;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Registers the DB Context to the Program.cs
+builder.Services.AddDbContext<DataContext>(opt =>
+    opt.UseNpgsql("CVEasyDB"));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CVEasy API v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
