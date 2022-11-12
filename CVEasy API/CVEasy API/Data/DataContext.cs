ea500b013f7657c936fff3d641c174591c250b62
UnchartedWorld <nathangvjcollins@gmail.com>
2022-11-12 23:55:51 +0000

    feat(DB Context): Provided connection to the database - known issues.
    This seems to cause *some* issue with connecting via localhost, and I've not found the issue just yet. However, I wanted to push this regardless. Fix should be coming soon.

--- CVEasy API/CVEasy API/Data/DataContext.cs
+++ CVEasy API/CVEasy API/Data/DataContext.cs
@@ -1,10 +1,21 @@
 using CVEasy_API.Model;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace CVEasy_API.Data;
 public class DataContext : DbContext
 {
+    private readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sql server with connection string from app settings
+        options.UseNpgsql(Configuration.GetConnectionString("CVEasyDB"));
+    }
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
     public DbSet<TableUser> Users { get; set; }
     public DbSet<TableUserDetails> UserDetails { get; set; }
