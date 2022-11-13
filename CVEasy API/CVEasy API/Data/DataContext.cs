35f559570e8c739a2dbca5cfc199c16b594f3a48
UnchartedWorld <nathangvjcollins@gmail.com>
2022-11-13 23:46:05 +0000

    refactor(DB Context): Change in name of models
    Essentially they mis-matched and this caused issues. Doing this solved that, which was good.

--- CVEasy API/CVEasy API/Data/DataContext.cs
+++ CVEasy API/CVEasy API/Data/DataContext.cs
@@ -18,12 +18,12 @@ public class DataContext : DbContext
     }  
     
     public DbSet<TableUser> TableUser { get; set; }
-    public DbSet<TableUserDetails> UserDetails { get; set; }
-    public DbSet<TableUserCategory> UserCategories { get; set; }
-    public DbSet<TableUserRole> UserRoles { get; set; }
-    public DbSet<TableCategoryDetails> CategoryDetails { get; set; }
-    public DbSet<TableThemes> Themes { get; set; }
+    public DbSet<TableUserDetails> TableUserDetails { get; set; }
+    public DbSet<TableUserCategory> TableUserCategories { get; set; }
+    public DbSet<TableUserRole> TableUserRoles { get; set; }
+    public DbSet<TableCategoryDetails> TableCategoryDetails { get; set; }
+    public DbSet<TableThemes> TableThemes { get; set; }
     public DbSet<TableComments> TableComments { get; set; }
-    public DbSet<TableTags> Tags { get; set; }
+    public DbSet<TableTags> TableTags { get; set; }
 
 }
