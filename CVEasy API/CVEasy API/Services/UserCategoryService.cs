6830bc1281f3d72154d1bf9a74487d7661ad4711
UnchartedWorld <nathangvjcollins@gmail.com>
2022-11-13 23:50:47 +0000

    feat(Service): Creation of service for User Category
    Not much to say, subject line says it all.

---
+++ CVEasy API/CVEasy API/Services/UserCategoryService.cs
@@ -0,0 +1,21 @@
+using CVEasy_API.Data;
+using CVEasy_API.Model;
+
+namespace CVEasy_API.Services;
+using Interfaces;
+
+public class UserCategoryService : IUserCategory
+{
+    private DataContext _dataContext;
+
+    public UserCategoryService(DataContext data)
+    {
+        _dataContext = data;
+    }
+
+    public List<TableUserCategory> GetUserCategory()
+    {
+        var result = _dataContext.TableUserCategories.ToList();
+        return result;
+    }
+}
