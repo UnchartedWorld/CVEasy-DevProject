3c7c9dfdfe9cebd94a41557079a4bad850884dc1
UnchartedWorld <nathangvjcollins@gmail.com>
2022-11-14 20:16:39 +0000

    feat(DTO): Constructor for DTO
    This includes a constructor for use in the controller, and makes the PK have the ability to be set (can be revoked if needed). Additionally, removed the [Key] call above the PK.

--- CVEasy API/CVEasy API/DTOs/UserListResponse.cs
+++ CVEasy API/CVEasy API/DTOs/UserListResponse.cs
@@ -4,8 +4,14 @@ namespace CVEasy_API.DTOs;
 
 public class UserListResponse
 {
-    [Key]
-    public int userID { get; }
+    public int userID { get; set; }
     public string loginName { get; set; }
     public string email { get; set; }
+
+    public UserListResponse (int id, string logName, string mail)
+    {
+        userID = id;
+        loginName = logName;
+        email = mail;
+    }
 }
