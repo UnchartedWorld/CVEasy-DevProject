ab728cc96a54153eca3a0849d784f08de891022d
UnchartedWorld <nathangvjcollins@gmail.com>
2022-11-14 20:18:06 +0000

    fix(Model): Fixed issue with DTO, adds set
    This fixed the issue where the DTO couldn't find the PK, which seemed to be spawned from not being able to set what value was the PK. A simple fix, but necessary.

--- CVEasy API/CVEasy API/Model/TableUser.cs
+++ CVEasy API/CVEasy API/Model/TableUser.cs
@@ -5,7 +5,7 @@ namespace CVEasy_API.Model;
 public class TableUser
 {
     [Key]
-    public int userID { get; }
+    public int userID { get; set;  }
     public string loginName { get; set; }
     public string passwordHash { get; set; }
     public string passwordSalt { get; set; }
