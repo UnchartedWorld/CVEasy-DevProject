a1b0059a0688d8cd85109db5203e6fb750d132e6
UnchartedWorld <nathangvjcollins@gmail.com>
2022-11-16 23:07:17 +0000

    fix(Service): Implementation of interfaces into service
    This was missing the connection to the interface, which unsurprisingly ruined the GET response.

--- CVEasy API/CVEasy API/Services/ThemesService.cs
+++ CVEasy API/CVEasy API/Services/ThemesService.cs
@@ -1,9 +1,11 @@
+using CVEasy_API.Controllers;
 using CVEasy_API.Data;
+using CVEasy_API.Interfaces;
 using CVEasy_API.Model;
 
 namespace CVEasy_API.Services;
 
-public class ThemesService
+public class ThemesService : IThemes
 {
     private DataContext _dataContext; // Initiates a new data context each time it's called. 
 
