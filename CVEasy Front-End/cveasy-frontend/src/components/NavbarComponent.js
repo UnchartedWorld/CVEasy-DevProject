7875d4c7ec55c2e8270dc85c560d769166f8598e
UnchartedWorld <nathangvjcollins@gmail.com>
2023-01-17 01:09:53 +0000

    feat(navbar): Basic responsive navigation bar implemented
    This implements the navigation bar using the pre-built MUI responsive navigation bar, which also manages to send people to the right place i.e. login page.

---
+++ CVEasy Front-End/cveasy-frontend/src/components/NavbarComponent.js
@@ -0,0 +1,178 @@
+import * as React from 'react';
+import {brandPrimary, brandSecondary} from "../CustomColors";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import {ThemeProvider, createTheme} from "@mui/material/styles";
+import {Link} from "react-router-dom";
+
+const pages = ['Home', 'Templates', 'About', 'Login'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: brandPrimary[400],
+            dark: brandPrimary[600]
+        },
+        secondary: {
+            main: brandSecondary[300],
+            dark: brandSecondary[500]
+        }
+    }
+});
+
+export const NavbarComponent = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: {xs: 'none', md: 'flex'},
+                                fontFamily: 'sans-serif',
+                                fontWeight: 700,
+                                flexGrow: 1,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}>
+                            CVEasy
+                        </Typography>
+
+                        <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit">
+                                <MenuIcon/>
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: {xs: 'block', md: 'none'},
+                                }}>
+                                {pages.map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">
+                                            <Link style={{textDecoration: "none", color: "inherit"}}
+                                                  to={`/${page}`}>{page}</Link>
+                                        </Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: {xs: 'flex', md: 'none'},
+                                flexGrow: 1,
+                                fontFamily: 'sans-serif',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}>
+                            CVEasy
+                        </Typography>
+                        <Box sx={{flexGrow: 0, display: {xs: 'none', md: 'flex'}}}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{my: 2, color: 'inherit', display: 'block'}}>
+                                    <Link style={{textDecoration: "none", color: "inherit"}}
+                                          to={`/${page}`}>{page}</Link>
+                                </Button>
+                            ))}
+                        </Box>
+
+                        <Box sx={{flexGrow: 0}}>
+                            <Tooltip title="Open user settings">
+                                <IconButton color='inherit' onClick={handleOpenUserMenu}
+                                            sx={{p: 0, marginLeft: 2, marginBottom: 0.3}}>
+                                    <AccountCircle/>
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{mt: '45px'}}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}>
+                                {settings.map((setting) => (
+                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center">{setting}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
+    )
+}
