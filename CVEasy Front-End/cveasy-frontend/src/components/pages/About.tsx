2ebf998b46e2607dfe4bea0b8ac8454fe00f07a3
UnchartedWorld <nathangvjcollins@gmail.com>
2023-01-22 22:48:34 +0000

    feat(about): Creation of About page
    Much like the Landing page, this just implements text to make sure the page loads in.

---
+++ CVEasy Front-End/cveasy-frontend/src/components/pages/About.tsx
@@ -0,0 +1,17 @@
+import {Typography} from "@mui/material";
+
+export default function About() {
+
+    return (
+        <main>
+            <Typography
+                variant={"h1"}
+                component={"h1"}
+                align={"left"}
+                color={"inherit"}
+                padding={"10px auto"}>
+                About this website
+
+            </Typography>
+        </main>)
+}
