ccbaa4cb6a75c85550bb8a7b1f99ba63f4a5a73f
UnchartedWorld <nathangvjcollins@gmail.com>
2023-01-24 23:05:58 +0000

    feat(service): Alters GetUser function
    As stated in the related interface commit, this makes sure to only hold onto the singular user that relates to what we searched for.

--- CVEasy API/CVEasy API/Services/UserService.cs
+++ CVEasy API/CVEasy API/Services/UserService.cs
@@ -1,20 +1,30 @@
 using CVEasy_API.Data;
 using CVEasy_API.DTOs;
+using CVEasy_API.Model;
 
 namespace CVEasy_API.Services;
+
 using Interfaces;
 
 public class UserService : IUser // This can be re-used to get all users in other interfaces/classes - OOP baby.
 {
     private DataContext _dataContext; // Initiates a new data context each time it's called. 
+
     public UserService(DataContext data) // Constructor
     {
-        _dataContext = data; 
+        _dataContext = data;
     }
 
-    public List<UserListResponse> GetUser()
+    public UserListResponse? GetUser(UserLoginRequest loginRequest)
     {
-        var dtoList = _dataContext.TableUser.Select(s => new UserListResponse(s.userID, s.loginName, s.email)).ToList();
-        return dtoList; // Returns it.
+        var dtoCheck =
+            _dataContext.TableUser
+                .Where
+                    (user => user.email == loginRequest.Email || user.loginName == loginRequest.Username)
+                .Select
+                    (s => new UserListResponse(s.userID, s.loginName, s.email))
+                .FirstOrDefault();
+
+        return dtoCheck; // Returns it.
     }
 }
