c3c2d4cc624228b4839d22c64993ee66d5f096c1
UnchartedWorld <nathangvjcollins@gmail.com>
2023-01-24 23:08:37 +0000

    refactor(dto): Refactors variable names
    Changes variable names to adhere better to .NET naming conventions

--- CVEasy API/CVEasy API/DTOs/UserListResponse.cs
+++ CVEasy API/CVEasy API/DTOs/UserListResponse.cs
@@ -1,17 +1,15 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace CVEasy_API.DTOs;
 
 public class UserListResponse
 {
-    public int userID { get; set; }
-    public string loginName { get; set; }
-    public string email { get; set; }
+    public int UserId { get; set; }
+    public string LoginName { get; set; }
+    public string Email { get; set; }
 
     public UserListResponse (int id, string logName, string mail)
     {
-        userID = id;
-        loginName = logName;
-        email = mail;
+        UserId = id;
+        LoginName = logName;
+        Email = mail;
     }
 }
