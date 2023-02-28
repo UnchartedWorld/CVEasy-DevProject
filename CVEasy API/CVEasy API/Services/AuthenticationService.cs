ffbbf39f57e22bbac6909a8b3bfe97e4fcabdcd0
UnchartedWorld <nathangvjcollins@gmail.com>
2023-02-28 03:02:45 +0000

    feat(service): Adds registration function, makes password verification public
    This allows a user to register with minor checks, and also allows me to verify a user by comparing a given password to the correct hash and salt in the DB. Also, gets the data context to make it all possible.

--- CVEasy API/CVEasy API/Services/AuthenticationService.cs
+++ CVEasy API/CVEasy API/Services/AuthenticationService.cs
@@ -1,14 +1,23 @@
 using System.Security.Cryptography;
 using System.Text;
+using CVEasy_API.Data;
+using CVEasy_API.Interfaces;
+using CVEasy_API.Model;
 
 namespace CVEasy_API.Services;
 
 //https://code-maze.com/csharp-hashing-salting-passwords-best-practices/ - Uses this guide.
-public class AuthenticationService
+public class AuthenticationService : IAuthentication
 {
     private const int KeySize = 64;
     private const int Iterations = 350000;
     private readonly HashAlgorithmName _hashAlgorithm = HashAlgorithmName.SHA512;
+    private DataContext _dataContext;
+
+    public AuthenticationService(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
 
     string HashUserPassword(string password, out byte[] salt)
     {
@@ -24,11 +33,27 @@ public class AuthenticationService
         return Convert.ToHexString(hash);
     }
 
-    bool VerifyUserPassword(string password, string hash, byte[] salt)
+    public bool VerifyUserPassword(string password, string hash, byte[] salt)
     {
         var compareHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, _hashAlgorithm, KeySize);
 
         return compareHash.SequenceEqual(Convert.FromHexString(hash));
     }
-    
+
+
+    //
+    public void RegisterUser(UserRegistrationRequest registrationRequest)
+    {
+        var passwordAfterHash = HashUserPassword(registrationRequest.Password, out var salt);
+
+        var newUser = new TableUser
+        {
+            loginName = registrationRequest.Username,
+            email = registrationRequest.Email,
+            passwordHash = passwordAfterHash,
+            passwordSalt = Convert.ToBase64String(salt)
+        };
+        _dataContext.TableUser.Add(newUser);
+        _dataContext.SaveChanges();
+    }
 }
