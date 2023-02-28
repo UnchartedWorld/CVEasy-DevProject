80ec24fb191931f2df60e9d48169db9842b72328
UnchartedWorld <nathangvjcollins@gmail.com>
2023-02-28 03:57:23 +0000

    refactor(model): Changes registration to require username, requests password once
    This is done as the front-end can handle the checking of password entries.

--- CVEasy API/CVEasy API/Model/UserRegistrationRequest.cs
+++ CVEasy API/CVEasy API/Model/UserRegistrationRequest.cs
@@ -4,10 +4,10 @@ namespace CVEasy_API.Model;
 
 public class UserRegistrationRequest
 {
+    [Required] 
+    public string Username { get; set; } = string.Empty;
     [Required, EmailAddress] 
     public string Email { get; set; } = string.Empty;
     [Required, MinLength(8, ErrorMessage = "Password can be no shorter than 8 characters.")]
     public string Password { get; set; } = string.Empty;
-    [Required, Compare("Password")]
-    public string ConfirmPassword { get; set; } = string.Empty;
 }
