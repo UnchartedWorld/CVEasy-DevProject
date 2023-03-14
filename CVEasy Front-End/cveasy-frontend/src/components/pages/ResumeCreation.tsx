839841fd1e141dfa5e2e7844241482c6d3104aa3
UnchartedWorld <nathangvjcollins@gmail.com>
2023-03-14 22:41:13 +0000

    feat(ui): Adds "ResumeCreation" page
    This is what "SurveyCreation" refers to. Currently contains just the editor, it will eventually contain the editor AND the PDF view.

---
+++ CVEasy Front-End/cveasy-frontend/src/components/pages/ResumeCreation.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography, Grid } from "@mui/material";
+import LaTeXAce from "components/LaTeXAce";
+
+export default function ResumeCreation() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} padding={"1rem"}>
+        <Grid item xs={6}>
+          <LaTeXAce />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
