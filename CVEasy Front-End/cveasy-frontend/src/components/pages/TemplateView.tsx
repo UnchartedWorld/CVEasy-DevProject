d49af75c9911c7541d87c2d0eae4322c37112285
UnchartedWorld <nathangvjcollins@gmail.com>
2023-03-14 22:45:59 +0000

    feat(ui): Implements TemplateInfo component, shows placeholder image
    This isn't complete, but it at least provides a basic idea of what I want this to look like in the future.

--- CVEasy Front-End/cveasy-frontend/src/components/pages/TemplateView.tsx
+++ CVEasy Front-End/cveasy-frontend/src/components/pages/TemplateView.tsx
@@ -1,23 +1,26 @@
-import { Typography, Container } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import TemplateInfo from "components/TemplateInfo";
+import data from './Templates';
 
-export default function TemplateView () {
-    
-    return (
-        <Container disableGutters maxWidth="md" component="main" sx={{ pt: 8, pb: 6 }}>
-            <Typography
-                component="h1"
-                variant="h2"
-                align="center"
-                color="text.primary"
-                gutterBottom
-            >
-                The Theme
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" component="p">
-                This is currently just a placeholder. This exists to allow me to ensure this page functions, but will 
-                eventually be converted into a more functional version of the template view.
-            </Typography>
-        </Container>
-    )
-    
+export default function TemplateView() {
+  return (
+    <Box>
+      <Grid container spacing={2} padding={"3rem"}>
+        <Grid item xs={6}>
+          <TemplateInfo />
+        </Grid>
+
+        <Grid item xs={6}>
+          <Box
+            height={"100%"}
+            width={"100%"}
+            component="img"
+            padding={4}
+            alt="Stuff"
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.kinja-img.com%2Fgawker-media%2Fimage%2Fupload%2Fs--KqMl6Djd--%2Fc_fill%2Cfl_progressive%2Cg_center%2Ch_900%2Cq_80%2Cw_1600%2F18mib01sr6u5ejpg.jpg&f=1&nofb=1&ipt=afbe8aa7e3ba83c9de26f66e12026bfb31931224c0ac4df7ca70dcd7049dfc88&ipo=images"
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
