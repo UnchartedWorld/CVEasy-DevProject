7ce6ed44bab6027f20acbb7a6c8678893e40d096
UnchartedWorld <nathangvjcollins@gmail.com>
2023-03-14 22:42:33 +0000

    feat(ui): Initiates LaTeXAce, the component holding the LaTeX editor
    As said above, this contains the element that is the editor used to handle LaTeX editing. I deemed it better to utilise this than create my own editor, as it would save time and still looks acceptable.

---
+++ CVEasy Front-End/cveasy-frontend/src/components/LaTeXAce.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/system";
+import "ace-builds/src-noconflict/ace";
+import "ace-builds/src-noconflict/snippets/latex";
+import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/theme-monokai";
+import AceEditor from "react-ace";
+
+
+export default function LaTeXAce() {
+    return (
+    <Box>
+      <AceEditor
+        name="editor"
+        mode="latex"
+        value="test"
+        theme="monokai"
+        height="90vh"
+        width="48vw"
+        fontSize="1.1rem"
+        enableBasicAutocompletion={true}
+        enableLiveAutocompletion={true}
+        enableSnippets={true}
+      />
+    </Box>
+  );
+    }
