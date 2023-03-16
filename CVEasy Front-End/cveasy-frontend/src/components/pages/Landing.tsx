f799c5f777c416fa331d451102acc67238164b4e
UnchartedWorld <nathangvjcollins@gmail.com>
2023-03-16 01:11:56 +0000

    feat(ui): Implements Hero into landing page, labels box component with semantic label
    This is barebones thus far, but will become bigger as we fill out the landing page. Nice.

---
+++ CVEasy Front-End/cveasy-frontend/src/components/pages/Landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Hero from "components/Hero";
+
+export default function Landing() {
+  return (
+      <Box component={"main"}>
+        <Hero />
+      </Box>
+  );
+}
