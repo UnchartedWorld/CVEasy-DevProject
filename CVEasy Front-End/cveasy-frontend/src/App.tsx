52ef362e4d59b0cf2ca90ed09cdeceaef6bffa4e
UnchartedWorld <nathangvjcollins@gmail.com>
2023-03-16 00:58:24 +0000

    feat(app): Makes landing page the first page, calls for accessibility check library
    It also uses CSSBaseline, which I forgot to add. Without this, dark mode does nothing and doesn't change the background, which unsurprisingly makes everything look awful. Additionally, I'm using the axe-core library to handle accessibility checks, which is working fantastically.

--- CVEasy Front-End/cveasy-frontend/src/App.tsx
+++ CVEasy Front-End/cveasy-frontend/src/App.tsx
@@ -6,17 +6,27 @@ import NavbarComponent from 'components/NavbarComponent';
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 import TemplateView from "./components/pages/TemplateView";
+import Landing from 'components/pages/Landing';
 import {ThemeProvider} from "@mui/material/styles";
 import themes from './themes';
 import ResumeCreation from 'components/pages/ResumeCreation';
+import { CssBaseline } from '@mui/material';
 
 function App() {
+const ReactDOM = require('react-dom');
+
+if (process.env.NODE_ENV !== 'production') {
+  const axe = require('@axe-core/react');
+  axe(React, ReactDOM, 1000);
+}
     return (
         <React.Fragment>
             <ThemeProvider theme={themes}>
+                <CssBaseline />
                 <NavbarComponent />
                 <Routes>
-                    <Route path="/"/>
+                    <Route path="/" element={<Landing/>}/>
+                    <Route path="/Home" element={<Landing/>}/>
                     <Route path="/About" element={<About/>}/>
                     <Route path="/Templates" element={<Templates/>}/>
                     <Route path="/Login" element={<LoginPage/>}/>
