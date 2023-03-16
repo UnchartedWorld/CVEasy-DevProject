cfa564171b94fc92eea6738e5262beb5c5e80b61
UnchartedWorld <nathangvjcollins@gmail.com>
2023-03-16 01:12:44 +0000

    style(ui): Labels grid component with semantic label
    Considering each side will be a section, this is sufficient.

--- CVEasy Front-End/cveasy-frontend/src/components/pages/ResumeCreation.tsx
+++ CVEasy Front-End/cveasy-frontend/src/components/pages/ResumeCreation.tsx
@@ -3,7 +3,7 @@ import LaTeXAce from "components/LaTeXAce";
 
 export default function ResumeCreation() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box component={"main"} sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} padding={"1rem"}>
         <Grid item xs={6}>
           <LaTeXAce />
