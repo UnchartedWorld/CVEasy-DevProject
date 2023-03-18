bdd5c7406fde2b160cb3a4cc710a9c836723ae83
UnchartedWorld <nathangvjcollins@gmail.com>
2023-03-18 19:41:06 +0000

    feat(app): Adds routing to 404 page and Account page, ran Prettier
    As stated above, I've added a 404 page to account for the scenarios where the URL leads nowhere, and an account details page to update and add account details. Nice.

--- CVEasy Front-End/cveasy-frontend/src/App.tsx
+++ CVEasy Front-End/cveasy-frontend/src/App.tsx
@@ -1,42 +1,47 @@
-import * as React from 'react';
+import * as React from "react";
 import Templates from "./components/pages/Templates";
 import About from "./components/pages/About";
-import {Route, Routes} from 'react-router-dom';
-import NavbarComponent from 'components/NavbarComponent';
+import { Route, Routes } from "react-router-dom";
+import NavbarComponent from "components/NavbarComponent";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 import TemplateView from "./components/pages/TemplateView";
-import Landing from 'components/pages/Landing';
-import {ThemeProvider} from "@mui/material/styles";
-import themes from './themes';
-import ResumeCreation from 'components/pages/ResumeCreation';
-import { CssBaseline } from '@mui/material';
+import Landing from "components/pages/Landing";
+import { ThemeProvider } from "@mui/material/styles";
+import themes from "./themes";
+import ResumeCreation from "components/pages/ResumeCreation";
+import { CssBaseline } from "@mui/material";
+import AccountDetails from "components/pages/AccountDetails";
+import NotFoundPage from "components/pages/NotFoundPage";
 
 function App() {
-const ReactDOM = require('react-dom');
+  const ReactDOM = require("react-dom");
 
-if (process.env.NODE_ENV !== 'production') {
-  const axe = require('@axe-core/react');
-  axe(React, ReactDOM, 1000);
-}
-    return (
-        <React.Fragment>
-            <ThemeProvider theme={themes}>
-                <CssBaseline />
-                <NavbarComponent />
-                <Routes>
-                    <Route path="/" element={<Landing/>}/>
-                    <Route path="/Home" element={<Landing/>}/>
-                    <Route path="/About" element={<About/>}/>
-                    <Route path="/Templates" element={<Templates/>}/>
-                    <Route path="/Login" element={<LoginPage/>}/>
-                    <Route path="/Register" element={<RegisterPage/>}/>
-                    <Route path="/TemplateView" element={<TemplateView/>}/>
-                    <Route path="/ResumeCreation" element={<ResumeCreation/>}/>
-                </Routes>
-            </ThemeProvider>
-        </React.Fragment>
-    )
+  if (process.env.NODE_ENV !== "production") {
+    const axe = require("@axe-core/react");
+    axe(React, ReactDOM, 1000);
+  }
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={themes}>
+        <CssBaseline />
+        <NavbarComponent />
+        <Routes>
+          {/* This handles the scenarios whereby a page isn't found. Handy for users, handy for me. */}
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/Home" element={<Landing />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Templates" element={<Templates />} />
+          <Route path="/Login" element={<LoginPage />} />
+          <Route path="/Register" element={<RegisterPage />} />
+          <Route path="/TemplateView" element={<TemplateView />} />
+          <Route path="/ResumeCreation" element={<ResumeCreation />} />
+          <Route path="/Account" element={<AccountDetails />} />
+        </Routes>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 export default App;
