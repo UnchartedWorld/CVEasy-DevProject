46779ece15519c28e21021c64876ebf7242da714
UnchartedWorld <nathangvjcollins@gmail.com>
2023-03-18 19:44:03 +0000

    feat(ui): Updates and incorporates landing page components
    By modularising it, I can edit one part without immediately affecting the others.

--- CVEasy Front-End/cveasy-frontend/src/components/pages/Landing.tsx
+++ CVEasy Front-End/cveasy-frontend/src/components/pages/Landing.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Hero from "components/Hero";
+import Features from "components/Features";
+import { brandPrimary } from "CustomColors";
+import InterestedComponent from "components/InterestedComponent";
 
 export default function Landing() {
   return (
-      <Box component={"main"}>
-        <Hero />
-      </Box>
+    <Box component={"main"}>
+      <Hero />
+      <Features />
+      <InterestedComponent />
+    </Box>
   );
 }
