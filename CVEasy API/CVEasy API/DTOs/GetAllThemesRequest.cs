0b805cd78659bdc5fc79a449e92ce3228343a4c4
UnchartedWorld <nathangvjcollins@gmail.com>
2023-04-06 22:35:47 +0100

    feat(dto): Adds GetAllThemesRequest
    This will provide search parameters, which will then let us include pagination.

---
+++ CVEasy API/CVEasy API/DTOs/GetAllThemesRequest.cs
@@ -0,0 +1,16 @@
+namespace CVEasy_API.DTOs
+{
+    public class GetAllThemesRequest
+    {
+        // these are your search parameters
+        public string createdByName { get; set; }
+        public string themeName { get; set; }
+        public List<int> tagIDs { get; set; }
+
+        // these are for paging
+        // pageIndex is for the current page the user is on
+        public int pageIndex { get; set; }
+        // pageSize is how many data is returned per page
+        public int pageSize { get; set; }
+    }
+}
