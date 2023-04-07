e255bb03f6f35f1dc9852e98ba0ce1e2e68df8b9
UnchartedWorld <nathangvjcollins@gmail.com>
2023-04-08 00:28:34 +0100

    feat(helper): Adds FileUpload helper
    This can both upload a file to the local system, and another method allows one to read the file at the stored location as a string.

---
+++ CVEasy API/CVEasy API/Helpers/FileUpload.cs
@@ -0,0 +1,31 @@
+namespace CVEasy_API.Helpers;
+
+public static class FileUpload
+{
+    public static string SaveTemplate(IFormFile submitFile)
+    {
+        if (!Directory.Exists("Templates"))
+        {
+            Directory.CreateDirectory("Templates");
+        }
+
+        var fileName = Guid.NewGuid();
+
+        var path = "Templates/" + fileName + ".tex";
+
+        using var stream = File.Create(path);
+        submitFile.CopyTo(stream);
+        return fileName.ToString();
+    }
+
+    public static string? ReadTemplate(string fileName)
+    {
+        if (!Directory.Exists("Templates/"))
+        {
+            return null;
+        }
+
+        var readText = File.ReadAllText("Templates/" + fileName + ".tex");
+        return readText;
+    }
+}
