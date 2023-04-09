ee497e35e72d62a2158eeb4aba3cec6f2cb9f54f
UnchartedWorld <nathangvjcollins@gmail.com>
2023-04-09 03:23:21 +0100

    feat(model): Adds string length to relevant fields
    Since I have a set amount of characters available for certain fields, it's best to limit what the user can input.

--- CVEasy API/CVEasy API/Model/UploadRequest.cs
+++ CVEasy API/CVEasy API/Model/UploadRequest.cs
@@ -4,9 +4,9 @@ namespace CVEasy_API.Model;
 
 public class UploadRequest
 {
-    [Required] public int UserID { get; }
-    [Required] public string ThemeTitle { get; set; } = string.Empty;
+    [Required] public int UserID { get; set; }
+    [Required][StringLength(300)] public string ThemeTitle { get; set; } = string.Empty;
     [Required] public IFormFile File { get; set; }
-    [Required] public string ThemeDescr { get; set; } = string.Empty;
-    [Required] public string ThemeVersion { get; set; } = string.Empty;
+    [Required][StringLength(5100)] public string ThemeDescr { get; set; } = string.Empty;
+    [Required][StringLength(30)] public string ThemeVersion { get; set; } = string.Empty;
 }
