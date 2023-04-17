1f48acc244d147f2892b73e875aae4cf63ae02ea
UnchartedWorld <nathangvjcollins@gmail.com>
2023-04-17 22:34:26 +0100

    feat(dto): Adds createdByUsername field to GetThemeResponse
    I want to know who made a template, but this will suffice. Also, this will get lost but I need to refactor the name to Template, not Theme

--- CVEasy API/CVEasy API/DTOs/GetThemeResponse.cs
+++ CVEasy API/CVEasy API/DTOs/GetThemeResponse.cs
@@ -5,6 +5,7 @@ namespace CVEasy_API.DTOs
         // rn, it's exactly like Table Theme class but it can be changed to whatever you want to return to FrontEnd
         public int themeID { get; set; }
         public int createdByID { get; set; }
+        public string createdByUsername { get; set; }
         public string themeName { get; set; }
         public string themeDescr { get; set; }
         public string themeFile { get; set; }
