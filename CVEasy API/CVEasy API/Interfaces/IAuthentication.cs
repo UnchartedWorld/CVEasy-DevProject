8a5eb3d0f7c1626d6d75b289441bb715fd1d0f96
UnchartedWorld <nathangvjcollins@gmail.com>
2023-04-17 03:05:22 +0100

    feat(interface): Adds GenerateJwtToken method to interface
    As stated above.

--- CVEasy API/CVEasy API/Interfaces/IAuthentication.cs
+++ CVEasy API/CVEasy API/Interfaces/IAuthentication.cs
@@ -6,4 +6,5 @@ public interface IAuthentication
 {
     public void RegisterUser(UserRegistrationRequest registrationRequest);
     public bool VerifyUserPassword(string userInputPassword, string userPasswordHash, byte[] userPasswordSalt);
+    public string GenerateJwtToken(UserListResponse user);
 }
