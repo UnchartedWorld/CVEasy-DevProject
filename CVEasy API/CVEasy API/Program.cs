0f709e53d23a26c1ed4e7a4432caca26d6140d42
UnchartedWorld <nathangvjcollins@gmail.com>
2023-04-17 03:14:01 +0100

    feat(program): Implements JWT middleware, keys and more
    This makes sure my JWT system, yanno, is recognised and works. Took a bit of bug-fixing, but works now.

--- CVEasy API/CVEasy API/Program.cs
+++ CVEasy API/CVEasy API/Program.cs
@@ -1,8 +1,11 @@
-using CVEasy_API.Controllers;
+using System.Text;
 using CVEasy_API.Data;
+using CVEasy_API.Helpers;
 using CVEasy_API.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using CVEasy_API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,10 +22,34 @@ var connectionString = builder.Configuration["ConnectionStrings:CVEasyDB"];
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(connectionString));
 
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Adds authentication service
+var key = Encoding.ASCII.GetBytes("YjjYgwP4JxbAbFHBCpPp");
+{
+    builder.Services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+
+}
 
 var app = builder.Build();
 
@@ -32,6 +59,9 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CVEasy API v1"));
+    
+    // Custom JWT authentication middleware
+    app.UseMiddleware<JwtMiddleware>();
 }
 
 app.UseHttpsRedirection();
