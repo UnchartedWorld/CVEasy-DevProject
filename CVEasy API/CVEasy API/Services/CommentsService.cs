4baba5ca79f67254215dfd05adff005a204851ae
UnchartedWorld <nathangvjcollins@gmail.com>
2023-04-18 16:28:43 +0100

    feat(services): Adds Update method, adds exception throw for Remove Comment
    As stated above, it adds a new method and updates an old one. Nice.

--- CVEasy API/CVEasy API/Services/CommentsService.cs
+++ CVEasy API/CVEasy API/Services/CommentsService.cs
@@ -1,5 +1,6 @@
 using CVEasy_API.Data;
 using CVEasy_API.DTOs;
+using CVEasy_API.Helpers;
 using CVEasy_API.Interfaces;
 using CVEasy_API.Model;
 
@@ -8,10 +9,12 @@ namespace CVEasy_API.Services;
 public class CommentsService : IComments
 {
     private DataContext _dataContext; // Initiates a new data context each time it's called. 
+    private IHttpContextAccessor _httpContextAccessor;
 
-    public CommentsService(DataContext commentsData)
+    public CommentsService(DataContext commentsData, IHttpContextAccessor httpContextAccessor)
     {
         _dataContext = commentsData;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public GetCommentPaging GetAllComments(GetAllCommentsRequest commentsRequest)
@@ -46,22 +49,57 @@ public class CommentsService : IComments
         _dataContext.TableComments.Add(newComment);
         _dataContext.SaveChanges();
     }
-    
+
     /// <summary>
     /// Changes a desired comment's body from whatever it was to [Deleted], similar to Reddit
     /// </summary>
     /// <param name="commentRemoveRequest">A model class representing the comment removal request</param>
     public void RemoveComment(CommentRemoveRequest commentRemoveRequest)
     {
-        var commentByUser = _dataContext.TableComments.FirstOrDefault((x =>
-            (x.userID == commentRemoveRequest.UserID && x.themeID == commentRemoveRequest.ThemeID &&
-             x.commentID == commentRemoveRequest.CommentId)));
+        AccountLogin userLogin = (AccountLogin)_httpContextAccessor.HttpContext.Items["UserLogin"];
+        
+        var commentByUser = _dataContext.TableComments.FirstOrDefault(x =>
+            x.userID == commentRemoveRequest.UserID && x.themeID == commentRemoveRequest.ThemeID &&
+            x.commentID == commentRemoveRequest.CommentId);
 
-        if (commentByUser == null) return;
+        if (commentByUser == null)
+        {
+            throw new Exception("User's comment not found. Yikes.");
+        }
+
+        if (commentByUser.userID != userLogin?.Id)
+        {
+            throw new Exception("You didn't make the original comment nor an admin, so you don't get to remove it.");
+        }
 
         commentByUser.comment = "[Deleted]";
 
         _dataContext.TableComments.Update(commentByUser);
         _dataContext.SaveChanges();
+    }
+
+    public void UpdateComment(CommentRequest commentRequest)
+    {
+        AccountLogin userLogin = (AccountLogin)_httpContextAccessor.HttpContext.Items["UserLogin"];
+        
+        var commentToUpdate = _dataContext.TableComments.FirstOrDefault(x =>
+            x.userID == commentRequest.UserID && x.themeID == commentRequest.ThemeID &&
+            x.commentID == commentRequest.CommentID);
+
+        if (commentToUpdate == null)
+        {
+            throw new Exception("Comment not found. How did you even do this?");
+        }
+
+        if (commentToUpdate.userID != userLogin?.Id)
+        {
+            throw new Exception("You didn't make the comment, so why are you trying to update it?");
+        }
+
+        commentToUpdate.comment = commentRequest.Comment;
+
+        _dataContext.TableComments.Update(commentToUpdate);
+        _dataContext.SaveChanges();
+
     }
 }
