1f35e93092c1aa59fe2d801b63fda0dce9b2b413
UnchartedWorld <nathangvjcollins@gmail.com>
2023-04-18 16:27:02 +0100

    fix(dto): Adds CommentID to CommentRequest
    Needed this for a few requests, so this solves that. Nice.

--- CVEasy API/CVEasy API/DTOs/CommentRequest.cs
+++ CVEasy API/CVEasy API/DTOs/CommentRequest.cs
@@ -7,4 +7,5 @@ public class CommentRequest
     [Required] public int UserID { get; set; }
     [Required] public int ThemeID { get; set; }
     [Required][StringLength(500)] public string Comment { get; set; } = string.Empty;
+    public int CommentID { get; set; }
 }
