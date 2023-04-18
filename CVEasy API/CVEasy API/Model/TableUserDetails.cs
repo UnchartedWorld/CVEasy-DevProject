08e08d9bf50e5177819c4e3aee698dade9fe29d6
UnchartedWorld <nathangvjcollins@gmail.com>
2023-04-18 23:14:17 +0100

    fix(model): Adds UserID field to TableUserDetails
    I entirely forgot to add this and fixed this for other models, but never touched this one until now.

--- CVEasy API/CVEasy API/Model/TableUserDetails.cs
+++ CVEasy API/CVEasy API/Model/TableUserDetails.cs
@@ -6,6 +6,7 @@ public class TableUserDetails
 {
     [Key]
     public int userDetailsID { get; set; }
+    public int userID { get; set; }
     public string firstName { get; set; }
     public string middleNames { get; set; }
     public string lastName { get; set; }
