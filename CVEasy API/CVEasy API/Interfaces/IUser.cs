452c498f2a4895678a70bf26b290b028d5be0503
UnchartedWorld <nathangvjcollins@gmail.com>
2023-05-02 03:51:19 +0100

    feat(interface): Adds user retrieval and detail response functions
    As stated above.

--- CVEasy API/CVEasy API/Interfaces/IUser.cs
+++ CVEasy API/CVEasy API/Interfaces/IUser.cs
@@ -6,6 +6,9 @@ namespace CVEasy_API.Interfaces;
 public interface IUser
 {
     public UserListResponse? GetUser(UserLoginRequest loginRequest);
+    public GetUserInfoResponse GetUserInfo(int userId);
+
+    public GetUserDetailsResponse GetUserDetails(int userId);
     public void UploadUserDetails(UserDetailsRequest detailsRequest);
     public void UpdateUserDetails(UserDetailsRequest detailsRequest);
 }
