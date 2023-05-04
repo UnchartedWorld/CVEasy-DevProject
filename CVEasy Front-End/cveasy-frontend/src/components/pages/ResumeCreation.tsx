53e0d06eef36d637a928c7a908ca3d5b24c905a1
UnchartedWorld <nathangvjcollins@gmail.com>
2023-05-04 21:08:37 +0100

    feat(ui): Incorporates PDF previewer, adds conditional text
    This now uses the PDFPreview component and ensures that it passes the needed data around. It also displays conditional text if the user hasn't generated anything.

--- CVEasy Front-End/cveasy-frontend/src/components/pages/ResumeCreation.tsx
+++ CVEasy Front-End/cveasy-frontend/src/components/pages/ResumeCreation.tsx
@@ -1,13 +1,42 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import { brandPrimary } from "CustomColors";
 import LaTeXAce from "components/LaTeXAce";
+import PDFPreview from "components/PDFPreview";
+import { useState } from "react";
 
 export default function ResumeCreation() {
+  const [pdfBlob, setPDFBlob] = useState<string | null>(null);
+
+  function handleCompilation(pdfBlob: any) {
+    if (pdfBlob) {
+      setPDFBlob(pdfBlob.toString());
+    }
+  }
+
   return (
     <Box component={"main"} sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} paddingTop={"1rem"} paddingBottom={"1rem"}>
-        <Grid item xs={12}>
-          <LaTeXAce />
+      <Grid container paddingTop={"1rem"} paddingBottom={"1rem"} sx={{backgroundColor: brandPrimary[200]}}>
+        <Grid item xs={6}>
+          <LaTeXAce onCompile={handleCompilation} />
         </Grid>
+        {pdfBlob ? (
+          <Grid item xs={6}>
+            <PDFPreview pdfURL={pdfBlob} />
+          </Grid>
+        ) : (
+          <Typography
+            component={"h4"}
+            variant={"h4"}
+            sx={{
+              marginTop: "auto",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginBottom: "auto",
+            }}
+          >
+            PDF not rendered, please try writing and compiling!
+          </Typography>
+        )}
       </Grid>
     </Box>
   );
