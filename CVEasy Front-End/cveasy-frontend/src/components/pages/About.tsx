bfc8cf889f351c513d2561e5683938361d6f5068
UnchartedWorld <nathangvjcollins@gmail.com>
2023-05-05 02:46:08 +0100

    feat(ui): Adds some text for the about page
    Truthfully this is entirely pointless, but I just didn't want it to be so empty.

--- CVEasy Front-End/cveasy-frontend/src/components/pages/About.tsx
+++ CVEasy Front-End/cveasy-frontend/src/components/pages/About.tsx
@@ -1,17 +1,38 @@
-import {Typography} from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 export default function About() {
-
-    return (
-        <main>
-            <Typography
-                variant={"h1"}
-                component={"h1"}
-                align={"left"}
-                color={"inherit"}
-                padding={"10px auto"}>
-                About this website
-
-            </Typography>
-        </main>)
+  return (
+    <Grid
+      container
+      component={"main"}
+      marginTop={"0.5rem"}
+      sx={{
+        minHeight: "80dvh",
+        padding: "2rem",
+      }}
+    >
+      <Grid item xs={12}>
+        <Typography
+          variant={"h1"}
+          component={"h1"}
+          align={"left"}
+          color={"inherit"}
+          padding={"10px auto"}
+          sx={{ fontSize: "4rem" }}
+        >
+          About this website
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="body1" component={"p"} sx={{ fontSize: "1.5rem" }}>
+          CVEasy is a ReactJS-based LaTeX compiling resume creator that allows
+          end users to, well, use LaTeX and make resumes, essays and whatever
+          else they feel like creating. It takes great inspiration from numerous
+          open source projects, alongside taking advantage of Texlive.js. It's
+          best described as being in alpha development, and shouldn't
+          realistically be used in any professional setting whatsoever.
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
